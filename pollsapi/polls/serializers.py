a37c341a7047da50a83596f289ca92c4f95133ca
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-09-02 14:35:56 +0400

    did vote and choise serializers

---
+++ pollsapi/polls/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Poll, Choice, Vote
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = '__all__'
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    votes = VoteSerializer(many=True, required=False)
+
+    class Meta:
+        model = Choice
+        fields = '__all__'
